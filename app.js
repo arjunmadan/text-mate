5a1b56fc9306e81fea1859721c8134b703180532
Arjun Madan <arjunmadan1992@gmail.com>
2016-08-02 20:05:51 -0400

    Fix for using environment variables

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var express   = require("express");
 var bandwidth = require("node-bandwidth");
 var app       = express();
 
-bandwidth.Client.globalOptions.apiToken = BANDWIDTH_CLIENT_API_TOKEN;
-bandwidth.Client.globalOptions.apiSecret = BANDWIDTH_CLIENT_API_SECRET;
-bandwidth.Client.globalOptions.userId = BANDWIDTH_CLIENT_USER_ID; 
+bandwidth.Client.globalOptions.apiToken  = process.env.BANDWIDTH_CLIENT_API_TOKEN;
+bandwidth.Client.globalOptions.apiSecret = process.env.BANDWIDTH_CLIENT_API_SECRET;
+bandwidth.Client.globalOptions.userId    = process.env.BANDWIDTH_CLIENT_USER_ID; 
 
 app.set('port', (process.env.PORT || 5000));
 
