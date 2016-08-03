d9b2d429b63850ddd0dd947f7dabdf37afb58c10
Arjun Madan <arjunmadan1992@gmail.com>
2016-08-02 20:09:56 -0400

    Add web interface, express settings

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 "use strict";
 
-var express   = require("express");
-var bandwidth = require("node-bandwidth");
-var app       = express();
+var express    = require("express");
+var bandwidth  = require("node-bandwidth");
+var bodyParser = require('body-parser');
+var app        = express();
 
 bandwidth.Client.globalOptions.apiToken  = process.env.BANDWIDTH_CLIENT_API_TOKEN;
 bandwidth.Client.globalOptions.apiSecret = process.env.BANDWIDTH_CLIENT_API_SECRET;
 bandwidth.Client.globalOptions.userId    = process.env.BANDWIDTH_CLIENT_USER_ID; 
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+
+app.get('/', function(request, response) {
+    response.send("Message received.");
+});
 
 app.post('/messages', function(request, response) {
-  console.log(JSON.stringify(request));
-  console.log(request.body.text);
-  response.send('Message received.');
+    console.log(JSON.stringify(request));
+    console.log(request.body.text);
+    response.send('Message received.');
 });
