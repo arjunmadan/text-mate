f6767c2093c14562886c656ff89ae3e70d1d5292
Arjun Madan <arjunmadan1992@gmail.com>
2016-08-04 19:50:40 -0400

    Use new node-bandwidth, test sms

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 "use strict";
 
-var express    = require("express");
-var bandwidth  = require("node-bandwidth");
-var bodyParser = require("body-parser");
-var app        = express();
-var context    = require("./lib/context");
+var express         = require("express");
+var CatapultClient  = require("node-bandwidth");
+var bodyParser      = require("body-parser");
+var app             = express();
+var context         = require("./lib/context");
 
-bandwidth.Client.globalOptions.apiToken  = process.env.BANDWIDTH_CLIENT_API_TOKEN;
-bandwidth.Client.globalOptions.apiSecret = process.env.BANDWIDTH_CLIENT_API_SECRET;
-bandwidth.Client.globalOptions.userId    = process.env.BANDWIDTH_CLIENT_USER_ID; 
+var client = new CatapultClient({
+    userId    : process.env.BANDWIDTH_CLIENT_USER_ID,
+    apiToken  : process.env.BANDWIDTH_CLIENT_API_TOKEN,
+    apiSecret : process.env.BANDWIDTH_CLIENT_API_SECRET
+});
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -19,7 +21,18 @@ app.use(bodyParser.urlencoded({
 
 app.post('/messages', function(request, response) {
     context.getIntents(request.body.text);
-    response.send('Message received.');
+    client.Message.send({
+        from : process.env.BANDWIDTH_PHONE_NUMBER, 
+        to   : request.body.from,
+        text : "Hello world."
+    })
+    .then(function(message) {
+        console.log("Message sent with ID " + message.id);
+        response.send(200);
+    })
+    .catch(function(err) {
+        console.log(err.message);
+    });
 });
 
 app.listen(app.get('port'), function() {
