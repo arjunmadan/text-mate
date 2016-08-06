3efe436420a7fbed4da909607f36b6e369b16c95
Arjun Madan <arjunmadan1992@gmail.com>
2016-08-06 14:26:44 -0400

    Fix s3

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-"use strict";
+'use strict';
 
-var express          = require("express");
-var CatapultClient   = require("node-bandwidth");
-var bodyParser       = require("body-parser");
-var context          = require("./lib/context");
-var imageRecognition = require("./lib/imageRecognition");
+var express          = require('express');
+var CatapultClient   = require('node-bandwidth');
+var bodyParser       = require('body-parser');
+var context          = require('./lib/context');
+var imageRecognition = require('./lib/imageRecognition');
+var aws              = require('aws-sdk');
 
 var client = new CatapultClient({
     userId    : process.env.BANDWIDTH_CLIENT_USER_ID,
     apiToken  : process.env.BANDWIDTH_CLIENT_API_TOKEN,
     apiSecret : process.env.BANDWIDTH_CLIENT_API_SECRET
 });
+
+const s3 = new aws.S3();
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -44,14 +47,14 @@ app.post('/messages', function(request, response) {
                 text : result
             })
             .then(function(message) {
-                console.log("Message sent with ID " + message.id);
+                console.log('Message sent with ID ' + message.id);
                 response.send(200);
             })
             .catch(function(err) {
                 client.Message.send({
                     from : process.env.BANDWIDTH_PHONE_NUMBER, 
                     to   : request.body.from,
-                    text : "An error occurred."
+                    text : 'An error occurred.'
                 })
                 .then(function() {
                     response.status(500).send('This wasn\'t supposed to happen.');    
